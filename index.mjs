3cbf3d886eaf8c6f9caf22c67aae1321c4b0ca3d
Rafał Tadaszak <rafal@tadaszak.info>
2019-08-12 22:02:18 +0200

    Clean up Add basi

--- index.mjs
+++ index.mjs
@@ -1,14 +1,4 @@
 import LaravelTranslations from "./dist/LaravelTranslations.js";
 
-const lang =  LaravelTranslations.build({
-  dir: [
-    "../../soIT/Premio/Web/resources/lang",
-    {
-      path: "../../soIT/Premio/Web/app/Modules/*/resources/lang",
-      namespaceFromPath: "../../soIT/Premio/Web/app/Modules/[:namespace]/resources/lang"
-    }
-  ],
-  php: true,
-  json: true
-});
+const lang =  LaravelTranslations.build();
 console.log(lang);
